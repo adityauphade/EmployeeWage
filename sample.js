32752797e9803655616df9c9ebb6412226eb738f
Aditya Uphade <aditya.uphade@morningstar.com>
2021-08-06 10:16:31 +0530

    Use Case 1

---
+++ sample.js
@@ -0,0 +1,11 @@
+const is_absent = 0;
+
+let empcheck = Math.floor(Math.random() * 10) % 2;
+console.log(empcheck)
+
+if (empcheck == is_absent) {
+    console.log("Employee is Absent")
+}
+else{
+    console.log("Employee is present")
+}
